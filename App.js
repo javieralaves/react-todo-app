dd646650447e73ace12a68a04aa775b777baf357
Javier Alaves <jayalaves@gmail.com>
2020-07-03 11:14:12 +0200

    Replacing hardcoded values with props

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from "react"
 import TodoItem from "./TodoItem"
+import todosData from "./todosData"
 
 function App() {
+
+    const todoComponents = todosData.map(todo => <TodoItem key={todo.id} todo={todo}/>)
+
     return (
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todoComponents}
         </div>
     )
 }
