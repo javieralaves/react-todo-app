307b23e8ca526454b516a05dd5a3ad97b8f778e9
Javier Alaves <jayalaves@gmail.com>
2020-07-08 06:47:57 +0200

    Stateful component update

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 
-function App() {
-
-    const todoComponents = todosData.map(todo => <TodoItem key={todo.id} todo={todo}/>)
-
-    return (
-        <div className="todo-list">
-            {todoComponents}
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
 }
 
 export default App
